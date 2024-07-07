3215af76cc785266273d26fcc320d4891d75d744
Param Kamlesh Pathak <parampathak47@gmail.com>
2024-07-07 17:25:57 +0530

    dsd

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-// // A faulty API route to test Sentry's error monitoring
-// export function GET() {
-//   throw new Error("Sentry Example API Route Error");
-//   return NextResponse.json({ data: "Testing Sentry Error..." });
-// }
+// A faulty API route to test Sentry's error monitoring
+export function GET() {
+  throw new Error("Sentry Example API Route Error");
+  return NextResponse.json({ data: "Testing Sentry Error..." });
+}
