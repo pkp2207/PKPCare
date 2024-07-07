6cfde76769bc5a23e888e9215f88dacc0704537a
Param Kamlesh Pathak <parampathak47@gmail.com>
2024-07-07 16:50:07 +0530

    sentry-errors

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "javascript-mastery",
-project: "care-pulse",
+org: "svnit-ht",
+project: "pkp-care",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
