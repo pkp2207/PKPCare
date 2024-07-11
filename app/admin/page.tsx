7616bc061dd3915303ba9b78d5c02c51a3ba9204
Param Kamlesh Pathak <parampathak47@gmail.com>
2024-07-11 11:34:48 +0530

    sd

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -14,7 +14,7 @@ const AdminPage = async () => {
       <header className="admin-header">
         <Link href="/" className="cursor-pointer">
           <Image
-            src="/assets/icons/logo-full.svg"
+            src="/assets/icons/logo-full-pkp.png"
             height={32}
             width={162}
             alt="logo"
